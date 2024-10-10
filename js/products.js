75b8e8ae54652a847659c6cf755ac5806dfc9f1e
Anas Jarniji <jarnijianas@gmail.com>
2024-10-10 23:37:44 +0100

    Initial Commit

---
+++ js/products.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id: '1',
+        name: 'Eko Tribute ST100 Violette',
+        price: 129.99,
+        image: 'images/product1.png',
+        description: 'La Eko Tribute ST100 est une guitare électrique aux lignes intemporelles qui ont marqué toutes les générations de guitaristes depuis les années 1950.'
+    },
+    {
+        id: '2',
+        name: 'Eko Tribute DV300 Blue',
+        price: 199.99,
+        image: 'images/product2.png',
+        description: 'La Eko Tribute DV300 est une guitare au design emblématique, reconnaissable à ses deux cornes qui rendent hommage à une légende du rock.'
+    },
+    {
+        id: '3',
+        name: 'Gretsch G5422TG Electromatic Snowcrest Orange',
+        price: 899.99,
+        image: 'images/product3.png',
+        description: 'Remaniée en 2022, la guitare électrique GRETSCH G5422TG Electromatic Classic Hollow Body.'
+    }
+];
