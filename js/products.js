5801b189b59f125e964d4af6fb46cc67546c4be4
Anas Jarniji <jarnijianas@gmail.com>
2024-10-13 00:17:16 +0100

    solving some problemes in responcivity of the website

--- js/products.js
+++ js/products.js
@@ -19,5 +19,12 @@ const products = [
         price: 899.99,
         image: 'images/product3.png',
         description: 'Remaniée en 2022, la guitare électrique GRETSCH G5422TG Electromatic Classic Hollow Body.'
+    },
+    {
+        id: '4',
+        name: 'PRS CE24 Semi Hollow Satin Burnt Amber Ebony Fretboard ',
+        price: 1699.00,
+        image: 'images/product4.png',
+        desctiption: "Depuis l'aube du rock and roll, le claquement et la réponse provenant d'une guitare à manche vissé sont un élément essentiel du mixage. Ces attributs sont, à leur tour, devenus essentiels pour de nombreux joueurs qui tentent de raconter leur histoire à travers la musique. Le manche vissé du CE24 arbore une finition Pattern Thin et une finition nitro satinée pour une sensation naturelle et addictive. Pour maximiser davantage le son et la clarté inhérents à la guitare, le CE 24 est équipé de micros PRS 85/15, que Paul a conçus pour avoir une clarté remarquable ainsi que des aigus et des graves étendus. L'électronique se compose d'un contrôle de volume et de tonalité push/pull avec interrupteur à bascule à 3 voies pour un total de six sons classiques. Cette guitare particulière est dotée d'une touche en ébène, améliorant encore le claquement et le mordant du modèle PRS CE24. La guitare présente quelques petites bosses sur le corps et la finition satinée est usée et est devenue brillante près des micros."
     }
 ];
